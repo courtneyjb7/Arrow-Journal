fde3a7a14ac9d70eadabc8ab0b7c433d23bd623c
courtneyjb7 <72003830+courtneyjb7@users.noreply.github.com>
2022-05-13 12:49:50 -0700

    Profile (#42)
    * sign up visuals
    
    * signup
    
    * Routing
    
    * profile frontend
    
    * user creation and login with firebase
    
    * authentication and routing
    
    * merged monthly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import SignUp from './components/profile/SignUp.js';
+import Login from './components/profile/Login.js';
+import Profile from './components/profile/Profile.js';
+import { ChakraProvider } from "@chakra-ui/react";
 import StartPage from './components/authentication/StartPage.js';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Monthly from "./components/spreads/Monthly";
 import Daily from "./components/spreads/Daily";
 
-
 function App() {
   return (
     <ChakraProvider>
-      <Box>
       <Router>
-        <Routes>
-          <Route exact path="/" element={<StartPage/>} />
-           <Route path="monthly" element={<Monthly />} />
+          <Routes>
+          <Route path="/" element={<StartPage />}>
+            <Route path="monthly" element={<Monthly />} />
             <Route path="daily" element={<Daily />} />
-        </Routes>
-      </Router>
-      </Box>
-
+            <Route path="/sign-up" element={<SignUp/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element={<Profile name='BJ Klingenberg' email='bklingen@calpoly.edu' />}/>
+          </Route>
+           
+          </Routes>
+        </Router>
     </ChakraProvider>
   );
 }
